2ae3cb7102086069e9b9f17f550bfce1b7f42e17
gnzlbg <gonzalobg88@gmail.com>
2018-02-21 19:44:11 +0100

    update windows tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -200,6 +200,101 @@ LBB14_15:
 .LBB14_15:
  pop     rbp
  ret"#
+    } else if cfg!(target_os = "windows") {
+    r#"lib_crate::sum_array (src\lib.rs:6):
+ push    rbp
+ mov     rbp, rsp
+ test    rdx, rdx
+ je      .LBB14_1
+ lea     r8, [4*rdx, -, 4]
+ shr     r8, 2
+ add     r8, 1
+ cmp     r8, 8
+ jae     .LBB14_4
+ xor     eax, eax
+ mov     r8, rcx
+ jmp     .LBB14_13
+.LBB14_1:
+ xor     eax, eax
+ pop     rbp
+ ret
+.LBB14_4:
+ movabs  r9, 9223372036854775800
+ and     r9, r8
+ lea     r11, [r9, -, 8]
+ mov     rax, r11
+ shr     rax, 3
+ lea     r10d, [rax, +, 1]
+ and     r10d, 3
+ cmp     r11, 24
+ jae     .LBB14_6
+ pxor    xmm0, xmm0
+ xor     eax, eax
+ pxor    xmm1, xmm1
+ test    r10, r10
+ jne     .LBB14_9
+ jmp     .LBB14_11
+.LBB14_6:
+ lea     r11, [r10, -, 1]
+ sub     r11, rax
+ pxor    xmm0, xmm0
+ xor     eax, eax
+ pxor    xmm1, xmm1
+.LBB14_7:
+ movdqu  xmm2, xmmword, ptr, [rcx, +, 4*rax]
+ paddd   xmm2, xmm0
+ movdqu  xmm0, xmmword, ptr, [rcx, +, 4*rax, +, 16]
+ paddd   xmm0, xmm1
+ movdqu  xmm1, xmmword, ptr, [rcx, +, 4*rax, +, 32]
+ movdqu  xmm3, xmmword, ptr, [rcx, +, 4*rax, +, 48]
+ movdqu  xmm4, xmmword, ptr, [rcx, +, 4*rax, +, 64]
+ paddd   xmm4, xmm1
+ paddd   xmm4, xmm2
+ movdqu  xmm2, xmmword, ptr, [rcx, +, 4*rax, +, 80]
+ paddd   xmm2, xmm3
+ paddd   xmm2, xmm0
+ movdqu  xmm0, xmmword, ptr, [rcx, +, 4*rax, +, 96]
+ paddd   xmm0, xmm4
+ movdqu  xmm1, xmmword, ptr, [rcx, +, 4*rax, +, 112]
+ paddd   xmm1, xmm2
+ add     rax, 32
+ add     r11, 4
+ jne     .LBB14_7
+ test    r10, r10
+ je      .LBB14_11
+.LBB14_9:
+ lea     rax, [rcx, +, 4*rax]
+ add     rax, 16
+ neg     r10
+.LBB14_10:
+ movdqu  xmm2, xmmword, ptr, [rax, -, 16]
+ paddd   xmm0, xmm2
+ movdqu  xmm2, xmmword, ptr, [rax]
+ paddd   xmm1, xmm2
+ add     rax, 32
+ add     r10, 1
+ jne     .LBB14_10
+.LBB14_11:
+ paddd   xmm0, xmm1
+ pshufd  xmm1, xmm0, 78
+ paddd   xmm1, xmm0
+ pshufd  xmm0, xmm1, 229
+ paddd   xmm0, xmm1
+ movd    eax, xmm0
+ cmp     r8, r9
+ je      .LBB14_15
+ lea     r8, [rcx, +, 4*r9]
+.LBB14_13:
+ lea     rcx, [rcx, +, 4*rdx]
+.LBB14_14:
+ add     eax, dword, ptr, [r8]
+ add     r8, 4
+ cmp     rcx, r8
+ jne     .LBB14_14
+.LBB14_15:
+ pop     rbp
+ ret
+"#
     } else {
         ""
     };
@@ -265,6 +360,32 @@ LBB0_1:
  pop     rbp
  ret
 "#
+    } else if cfg!(target_os = "windows") {
+        r#"lib_crate::bar::max_array (src\bar.rs:3):
+ push    rbp
+ mov     rbp, rsp
+ mov     rax, -524288
+.LBB0_1:
+ movupd  xmm0, xmmword, ptr, [rdx, +, rax, +, 524288]
+ movupd  xmm1, xmmword, ptr, [rdx, +, rax, +, 524304]
+ movupd  xmm2, xmmword, ptr, [rcx, +, rax, +, 524288]
+ maxpd   xmm0, xmm2
+ movupd  xmm2, xmmword, ptr, [rcx, +, rax, +, 524304]
+ movupd  xmm3, xmmword, ptr, [rcx, +, rax, +, 524320]
+ movupd  xmm4, xmmword, ptr, [rcx, +, rax, +, 524336]
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524288], xmm0
+ maxpd   xmm1, xmm2
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524304], xmm1
+ movupd  xmm0, xmmword, ptr, [rdx, +, rax, +, 524320]
+ maxpd   xmm0, xmm3
+ movupd  xmm1, xmmword, ptr, [rdx, +, rax, +, 524336]
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524320], xmm0
+ maxpd   xmm1, xmm4
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524336], xmm1
+ add     rax, 64
+ jne     .LBB0_1
+ pop     rbp
+ ret"#
     } else {
         ""
     };
@@ -482,6 +603,109 @@ LBB14_15:
  }
  pop     rbp
  ret"#
+    } else if cfg!(target_os = "windows") {
+        r#"pub fn sum_array(x: &[i32]) -> i32 {
+ push    rbp
+ mov     rbp, rsp
+     if self.ptr == self.end { (libcore\slice\mod.rs:1178)
+     test    rdx, rdx
+     je      .LBB14_1
+     lea     r8, [4*rdx, -, 4]
+     shr     r8, 2
+     add     r8, 1
+     cmp     r8, 8
+     jae     .LBB14_4
+     xor     eax, eax
+     mov     r8, rcx
+     jmp     .LBB14_13
+.LBB14_1:
+ }
+ xor     eax, eax
+ pop     rbp
+ ret
+.LBB14_4:
+     if self.ptr == self.end { (libcore\slice\mod.rs:1178)
+     movabs  r9, 9223372036854775800
+     and     r9, r8
+     lea     r11, [r9, -, 8]
+     mov     rax, r11
+     shr     rax, 3
+     lea     r10d, [rax, +, 1]
+     and     r10d, 3
+     cmp     r11, 24
+     jae     .LBB14_6
+     pxor    xmm0, xmm0
+     xor     eax, eax
+     pxor    xmm1, xmm1
+     test    r10, r10
+     jne     .LBB14_9
+     jmp     .LBB14_11
+.LBB14_6:
+     lea     r11, [r10, -, 1]
+     sub     r11, rax
+     pxor    xmm0, xmm0
+     xor     eax, eax
+     pxor    xmm1, xmm1
+.LBB14_7:
+ x.iter().fold(0, |sum, next| sum + *next)
+ movdqu  xmm2, xmmword, ptr, [rcx, +, 4*rax]
+ paddd   xmm2, xmm0
+ movdqu  xmm0, xmmword, ptr, [rcx, +, 4*rax, +, 16]
+ paddd   xmm0, xmm1
+ movdqu  xmm1, xmmword, ptr, [rcx, +, 4*rax, +, 32]
+ movdqu  xmm3, xmmword, ptr, [rcx, +, 4*rax, +, 48]
+ movdqu  xmm4, xmmword, ptr, [rcx, +, 4*rax, +, 64]
+ paddd   xmm4, xmm1
+ paddd   xmm4, xmm2
+ movdqu  xmm2, xmmword, ptr, [rcx, +, 4*rax, +, 80]
+ paddd   xmm2, xmm3
+ paddd   xmm2, xmm0
+ movdqu  xmm0, xmmword, ptr, [rcx, +, 4*rax, +, 96]
+ paddd   xmm0, xmm4
+ movdqu  xmm1, xmmword, ptr, [rcx, +, 4*rax, +, 112]
+ paddd   xmm1, xmm2
+ add     rax, 32
+ add     r11, 4
+ jne     .LBB14_7
+ test    r10, r10
+ je      .LBB14_11
+.LBB14_9:
+ lea     rax, [rcx, +, 4*rax]
+ add     rax, 16
+ neg     r10
+.LBB14_10:
+ movdqu  xmm2, xmmword, ptr, [rax, -, 16]
+ paddd   xmm0, xmm2
+ movdqu  xmm2, xmmword, ptr, [rax]
+ paddd   xmm1, xmm2
+ add     rax, 32
+ add     r10, 1
+ jne     .LBB14_10
+.LBB14_11:
+ paddd   xmm0, xmm1
+ pshufd  xmm1, xmm0, 78
+ paddd   xmm1, xmm0
+ pshufd  xmm0, xmm1, 229
+ paddd   xmm0, xmm1
+ movd    eax, xmm0
+ cmp     r8, r9
+     if self.ptr == self.end { (libcore\slice\mod.rs:1178)
+     je      .LBB14_15
+     lea     r8, [rcx, +, 4*r9]
+.LBB14_13:
+     lea     rcx, [rcx, +, 4*rdx]
+.LBB14_14:
+ add     eax, dword, ptr, [r8]
+     intrinsics::offset(self, count) (libcore\ptr.rs:622)
+     add     r8, 4
+     if self.ptr == self.end { (libcore\slice\mod.rs:1178)
+     cmp     rcx, r8
+     jne     .LBB14_14
+.LBB14_15:
+ }
+ pop     rbp
+ ret
+"#
     } else {
         ""
     };
@@ -549,6 +773,34 @@ LBB0_1:
  }
  pop     rbp
  ret"#
+    } else if cfg!(target_os = "windows") {
+        r#"pub fn max_array(x: &mut[f64; 65536], y: &[f64; 65536]) {
+ push    rbp
+ mov     rbp, rsp
+ mov     rax, -524288
+.LBB0_1:
+ x[i] = if y[i] > x[i] { y[i] } else { x[i] };
+ movupd  xmm0, xmmword, ptr, [rdx, +, rax, +, 524288]
+ movupd  xmm1, xmmword, ptr, [rdx, +, rax, +, 524304]
+ movupd  xmm2, xmmword, ptr, [rcx, +, rax, +, 524288]
+ maxpd   xmm0, xmm2
+ movupd  xmm2, xmmword, ptr, [rcx, +, rax, +, 524304]
+ movupd  xmm3, xmmword, ptr, [rcx, +, rax, +, 524320]
+ movupd  xmm4, xmmword, ptr, [rcx, +, rax, +, 524336]
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524288], xmm0
+ maxpd   xmm1, xmm2
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524304], xmm1
+ movupd  xmm0, xmmword, ptr, [rdx, +, rax, +, 524320]
+ maxpd   xmm0, xmm3
+ movupd  xmm1, xmmword, ptr, [rdx, +, rax, +, 524336]
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524320], xmm0
+ maxpd   xmm1, xmm4
+ movupd  xmmword, ptr, [rcx, +, rax, +, 524336], xmm1
+ add     rax, 64
+ jne     .LBB0_1
+ }
+ pop     rbp
+ ret"#
     } else {
         ""
     };
@@ -560,11 +812,8 @@ LBB0_1:
 
 #[test]
 fn trait_method() {
-    lib_test(&[
-        "<lib_crate::baz::Foo as lib_crate::baz::Addd>::addd",
-        "--rust",
-    ]).stdout()
-        .is(r#"fn addd(&self) -> usize {
+    let expected = if cfg!(target_os = "macos") || cfg!(target_os = "linux") {
+        r#"fn addd(&self) -> usize {
  push    rbp
  mov     rbp, rsp
  self.x + self.y
@@ -573,7 +822,26 @@ fn trait_method() {
  }
  pop     rbp
  ret
-"#)
+"#
+    } else if cfg!(target_os = "windows") {
+        r#"fn addd(&self) -> usize {
+ push    rbp
+ mov     rbp, rsp
+ self.x + self.y
+ mov     rax, qword, ptr, [rcx, +, 8]
+ add     rax, qword, ptr, [rcx]
+ }
+ pop     rbp
+ ret
+"#
+    } else {
+        ""
+    };
+    lib_test(&[
+        "<lib_crate::baz::Foo as lib_crate::baz::Addd>::addd",
+        "--rust",
+    ]).stdout()
+        .is(expected)
         .unwrap();
 }
 
@@ -593,9 +861,8 @@ fn generic_function() {
 
 #[test]
 fn inherent_method() {
-    lib_test(&["lib_crate::baz::Foo::foo_add", "--rust"])
-        .stdout()
-        .is(r#"pub fn foo_add(&self) -> usize {
+    let expected = if cfg!(target_os = "macos") || cfg!(target_os = "linux") {
+        r#"pub fn foo_add(&self) -> usize {
  push    rbp
  mov     rbp, rsp
  self.x + self.y
@@ -604,7 +871,24 @@ fn inherent_method() {
  }
  pop     rbp
  ret
-"#)
+"#
+    } else if cfg!(target_os = "windows") {
+        r#"pub fn foo_add(&self) -> usize {
+ push    rbp
+ mov     rbp, rsp
+ self.x + self.y
+ mov     rax, qword, ptr, [rcx, +, 8]
+ add     rax, qword, ptr, [rcx]
+ }
+ pop     rbp
+ ret
+"#
+    } else {
+        ""
+    };
+    lib_test(&["lib_crate::baz::Foo::foo_add", "--rust"])
+        .stdout()
+        .is(expected)
         .unwrap();
 }
 
