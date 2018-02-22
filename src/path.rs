be402cf0c256eedcdb2fbde43634c677a59e94d8
gnzlbg <gonzalobg88@gmail.com>
2018-02-22 19:50:18 +0100

    fix path::contains bug

--- src/path.rs
+++ src/path.rs
@@ -59,7 +59,9 @@ pub fn after(
         } else {
             if let Some(next_sp) = next_sub_path {
                 if let ::std::path::Component::RootDir = next_sp {
-                    next_sub_path = sub_path_iter.next();
+                    if !cfg!(target_os = "windows") {
+                        next_sub_path = sub_path_iter.next();
+                    }
                 }
             }
 
