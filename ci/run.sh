27ec8e7d0313c740757cd06ac7e47c2111745809
gnzlbg <gonzalobg88@gmail.com>
2018-02-24 11:31:26 +0100

    update project-path to manifest-path on ci

--- ci/run.sh
+++ ci/run.sh
@@ -10,5 +10,5 @@ rustup component add rust-src
 cargo build --release
 cargo test --release --target=$TARGET -- --nocapture
 
-cargo run --bin cargo-asm -- asm --project-path cargo-asm-test/lib_crate lib_crate::sum_array --rust --debug-mode
-cargo run --bin cargo-llvm-ir -- llvm-ir --project-path cargo-asm-test/lib_crate lib_crate::sum_array --debug-mode
+cargo run --bin cargo-asm -- asm --manifest-path cargo-asm-test/lib_crate lib_crate::sum_array --rust --debug-mode
+cargo run --bin cargo-llvm-ir -- llvm-ir --manifest-path cargo-asm-test/lib_crate lib_crate::sum_array --debug-mode
