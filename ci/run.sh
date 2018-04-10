1b126b293dd6c8db00ca85c9203f1234ebdff2b5
gnzlbg <gonzalobg88@gmail.com>
2018-04-10 14:25:12 +0200

    ci script should error hard quickly

--- ci/run.sh
+++ ci/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -ex
+
 : ${TARGET?"The TARGET environment variable must be set."}
 
 export RUST_TEST_THREADS=1
@@ -7,7 +9,7 @@ export RUST_BACKTRACE=1
 
 rustup component add rust-src
 
-cargo build --release
+cargo build --release --target=$TARGET
 cargo test --release --target=$TARGET -- --nocapture
 
 cargo run --bin cargo-asm -- asm --manifest-path cargo-asm-test/lib_crate lib_crate::sum_array --rust --debug-mode
