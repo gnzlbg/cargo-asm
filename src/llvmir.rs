7d0ece74657edb002bd8530227b829b31fd19dcd
gnzlbg <gonzalobg88@gmail.com>
2018-06-07 18:09:38 +0200

    improve demangling of LLVM-iR

--- src/llvmir.rs
+++ src/llvmir.rs
@@ -2,18 +2,23 @@ use options::*;
 
 pub fn run(files: &[::std::path::PathBuf]) {
     let mut function_table: Option<Vec<String>> = None;
+
     for f in files {
+        debug!("Scanning file: {:?}", f);;
         assert!(
             f.exists(),
             "path does not exist: {}",
             f.display()
         );
         let r = print_function(f);
-        if r.is_none() {
+
+        if r.is_ok() {
+            debug!("Function found, we are done!");
             function_table = None;
             break;
         }
-        let mut r = r.unwrap();
+        debug!("Function not found, appending all function names in the file to the table...");
+        let mut r = r.unwrap_err();
         if let Some(ref mut function_table) = function_table {
             function_table.append(&mut r);
         } else {
@@ -22,8 +27,11 @@ pub fn run(files: &[::std::path::PathBuf]) {
     }
 
     if function_table.is_none() {
+        debug!("Function found!");
         return;
     }
+
+    debug!("Function not found. Showing functions in the table...");
     let mut function_table = function_table.unwrap();
 
     match opts.path() {
@@ -75,7 +83,7 @@ Tips:
     }
 }
 
-fn print_function(file_name: &::std::path::PathBuf) -> Option<Vec<String>> {
+fn print_function(file_name: &::std::path::PathBuf) -> Result<(), Vec<String>> {
     use std::io::BufRead;
 
     let path = if let Some(path) = opts.path() { path } else { "".to_owned() };
@@ -90,9 +98,11 @@ fn print_function(file_name: &::std::path::PathBuf) -> Option<Vec<String>> {
 
         if let Some(ref mut function_lines) = function_lines {
             if line.starts_with("define") {
+                debug!("End of function");
                 break;
             }
 
+            debug!("    {:?}", line);
             function_lines.push(line);
         } else {
             if !line.starts_with("define") {
@@ -100,31 +110,62 @@ fn print_function(file_name: &::std::path::PathBuf) -> Option<Vec<String>> {
             }
 
             let first = line.find("@").unwrap();
-            let last = line.find("(").unwrap();
+            let last = (&line[first..]).find("(").unwrap() + first;
+            assert!(first < last, "first: {:?}, last: {:?}, line:\n{:?}", first, last, line);
             let mangled_name = &line[first + 1..last];
             let demangled_name = ::demangle::demangle(&mangled_name);
             if demangled_name != path {
                 function_names.push(demangled_name);
                 continue;
             }
-            function_lines = Some(vec![line.clone()]);
+            function_lines = Some(vec![line.replace(mangled_name, &demangled_name)]);
+            debug!("Found function with path: {:?}", path);
             continue;
         }
     }
 
     if let Some(function_lines) = function_lines {
+        debug!("Function found! Displaying function...");
         // Find last }
         let r = function_lines
             .iter()
             .rposition(|s| s.trim() == "}");
         let r = r.unwrap_or(function_lines.len() - 1);
-        for l in &function_lines[0..r + 1] {
-            println!("{}", l);
+        for line in &function_lines[0..r + 1] {
+            let mut demangled_line = String::new();
+            let mut start = 0;
+            while let Some(f) = &line[start..].find("\"") {
+                if start == 0 {
+                    debug!("line to demangle: {}", line);
+                }
+                debug!("s: {}, f: {}, dl: {}", start, f, demangled_line);
+                let f = f + start + 1;
+                let l = &line[f..].find("\"").unwrap() + f;
+                let mangled_name = &line[f..l];
+                let demangled_name = if mangled_name.ends_with(".exit") {
+                    let mut v = ::demangle::demangle(&mangled_name[0..mangled_name.len() - 5]);
+                    v.extend(".exit".chars());
+                    v
+                } else {
+                    ::demangle::demangle(&mangled_name)
+                };
+                debug!("  f: {}, l: {}, mn: {}, dm: {}", f, l, mangled_name, demangled_name);
+                demangled_line.extend(line[start..f].chars());
+                demangled_line.extend(demangled_name.chars());
+                demangled_line.extend("\"".chars());
+                start = l + 1;
+                debug!("  ns: {}, ndl: {}", start, demangled_line);
+            }
+            if demangled_line.is_empty() {
+                println!("{}", line);
+            } else {
+                println!("{}", demangled_line);
+            }
         }
-        None
+        Ok(())
     } else {
         function_names.sort();
         function_names.dedup();
-        Some(function_names)
+        Err(function_names)
     }
 }
